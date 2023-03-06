d9455f98c53fb20be6c6cbf09891705dc30695c7
liuanning <1302111164@163.com>
2023-03-06 16:18:05 +0800

    添加内容

--- main.py
+++ main.py
@@ -4,9 +4,16 @@
 # def print_hi(name):
 #     # 在下面的代码行中使用断点来调试脚本。
 #     print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+print(0)
 
 print(1)
 print(2)
+
+
+print(3)
+print(4)
+
+print(5)
 # 按间距中的绿色按钮以运行脚本。
 # if __name__ == '__main__':
 #     print_hi('PyCharm')
