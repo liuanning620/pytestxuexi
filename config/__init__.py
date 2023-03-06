c98c8b386b4d1908c94af918d4a5fa286ccd4189
liuanning <1302111164@163.com>
2023-03-06 16:08:29 +0800

    初始化

---
+++ config/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+"""
+@author: Alan
+@file: __init__.py.py
+@time: 2023/3/6 10:03
+@desc: 
+"""
---
+++ test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+"""
+@author: Alan
+@file: test.py
+@time: 2023/3/6 16:05
+@desc: 
+"""
+# 哈哈哈哈
